50fb1fe3f68b8beac92773799f430a3c2b278a37
magae1 <jjilil8351@gmail.com>
2025-07-17 21:54:49 +0900

    🎉 스프링 부트 시작

---
+++ src/test/kotlin/com/mysom/MysomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mysom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysomApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
