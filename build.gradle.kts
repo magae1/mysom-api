ef17c42975d3951104016a891034e71795976ed9
magae1 <jjilil8351@gmail.com>
2025-07-18 20:36:30 +0900

    🔧 기본 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
+    // 코틀린 lint. pre-commit hook 지원
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
 }
 
 group = "com"
@@ -24,15 +26,40 @@ repositories {
     mavenCentral()
 }
 
+val kotlinLoggerVersion = "7.0.3"
+val mysqlConnectorVersion = "9.3.0"
+val exposedVersion = "1.0.0-beta-4"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    /**
+     * kotlin-logging은 slf4j-api 의존성이 필요하지만,
+     * spring boot는 이미 slf4j-api 의존성을 포함하고 있다.
+     */
+    implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLoggerVersion")
+
+    // db connector[mysql]
+    implementation("com.mysql:mysql-connector-j:$mysqlConnectorVersion")
+
+    // exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
